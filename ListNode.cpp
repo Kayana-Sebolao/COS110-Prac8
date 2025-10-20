f36b45299fd73b2c22a6dd195b63238aa8f9640e
Kayana-Sebolao <167577382+Kayana-Sebolao@users.noreply.github.com>
2025-10-20 10:55:09 +0000

    .h file complete

--- ListNode.cpp
+++ ListNode.cpp
@@ -1,5 +1,27 @@
 #ifndef LISTNODE_CPP
 #define LISTNODE_CPP
+#include "ListNode.h"
+#include <iostream>
 
+template <class T>
+ListNode<T>::ListNode(T val): value(val), prev(NULL), next(NULL){}
+
+template <class T>
+ListNode<T>::~ListNode(){
+//not too sure
+}
+
+template <class T>
+T ListNode<T>::getValue() const{
+    return this->value;
+}
+
+template <class T>
+int ListNode<T>::size() const {
+    int numNodes = 0;
+    ListNode* nodePtr;
+    
+    while()
+}
 
 #endif //LISTNODE_CPP
